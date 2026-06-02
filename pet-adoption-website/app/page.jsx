8a27330444f92b06a89cac277fee42077632aac0
Britney839 <britney.harris84@ed.cna.nl.ca>
2026-06-02 12:15:13 -0230

    added website banner and start of NavBar

--- pet-adoption-website/app/page.jsx
+++ pet-adoption-website/app/page.jsx
@@ -3,7 +3,15 @@ export default function HomePage() {
   return (
     <div>
       <header>
-        <nav></nav>
+        <nav className="navbar">
+          <img src="banner.png" alt="Banner Logo" className="navbar-logo" width={2000} height={400}/>
+          <ul className ="navbar-menu">
+            <li>Home</li>
+            <li>Adopt</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </header>
 
       <main>
